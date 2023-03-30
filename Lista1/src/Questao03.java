00f018e6217e3dc9f45b2eb8c9ac965b05501b40
ArthurAgDuarte <duartearthur30@gmail.com>
2023-03-30 09:40:43 -0300

    Lista1

---
+++ Lista1/src/Questao03.java
@@ -0,0 +1,14 @@
+
+public class Questao03 {
+
+	public static void main(String[] args) {
+		int A= 10;
+		int B= 10;
+		int soma = A + B;
+		if(A==10 && B==10 && soma==20) 
+			System.out.println("A == 10" + "\n" + "B == 10"+ "\n" + "A + B == 20");
+		
+
+	}
+
+}
